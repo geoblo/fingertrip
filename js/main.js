9eca95f4d4b42b2400d97668e2a8a567b0306020
geoblo <geoblo@naver.com>
2022-04-06 18:05:55 +0900

    이미지 슬라이더 구현

---
+++ js/main.js
@@ -0,0 +1,17 @@
+// Slider
+const swiper = new Swiper('.swiper', {
+  loop: true,
+  // centeredSlides: true,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
