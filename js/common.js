eafa3ada552ad4d29b901ad044f67dd2c3cbae9e
geoblo <geoblo@naver.com>
2022-04-12 19:02:30 +0900

    모바일 반응형 메뉴 완성 및 버그 수정중

--- js/common.js
+++ js/common.js
@@ -1,5 +1,37 @@
-// Enable tooltips
-var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'));
-var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
-  return new bootstrap.Tooltip(tooltipTriggerEl)
+$(function() {
+  // Enable tooltips
+  let tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'));
+  let tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+    return new bootstrap.Tooltip(tooltipTriggerEl)
+  });
+
+  /*
+    모바일(태블릿)용 메뉴 보이기/숨기기
+  */
+  const nav = document.querySelector(".nav");
+
+  // 1. JS 사용
+  const btnHamburger = document.querySelector(".hamburger");
+  btnHamburger.addEventListener("click", () => {
+    nav.style.display = "block";
+  });
+
+  // 2. jQuery 사용
+  $(".close > .fa-xmark").on("click", () => {
+    $(".nav").hide();
+  });
+
+  /*
+    모바일(태블릿)용 메뉴에서 하위(서브)메뉴 보이기/숨기기
+  */
+  $("li.dropdown").click(() => {
+    $(this).find("ul.lnb").toggle();
+  });
+  
+  // $("li.dropdown").mouseover(() => {
+  //   $(this).find("ul.lnb").show();
+  // });
+  // $("li.dropdown").mouseout(() => {
+  //   $(this).find("ul.lnb").hide();
+  // });
 });
--- js/main.js
+++ js/main.js
@@ -1,17 +1,19 @@
-// Slider
-const swiper = new Swiper('.swiper', {
-  loop: true,
-  // centeredSlides: true,
-  autoplay: {
-    delay: 2500,
-    disableOnInteraction: false,
-  },
-  pagination: {
-    el: '.swiper-pagination',
-    clickable: true,
-  },
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
+$(function() {
+  // Slider
+  const swiper = new Swiper('.swiper', {
+    loop: true,
+    // centeredSlides: true,
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
 });
