467b78370fb4c4186d462e2e4a866225e50ae2d7
geoblo <geoblo@naver.com>
2022-04-15 18:30:39 +0900

    여행 코스 만들기 및 저장 로직 완성

--- js/map.js
+++ js/map.js
@@ -1,78 +1,120 @@
 let map;
+let mapOverview;
 let service;
+let geocoder;
 let infoWindow;
-let markers = [];
+let marker;
+let markers = []; // 마커 정보를 배열에 담음
+var markersFinal = []; // 최종 마커 정보를 배열에 담음
+let lineSymbol;
+
+let path = [];
+let poly;
+let polys = []; // 위도, 경도 정보를 배열에 담음
+let polyIndex = 0;
+
+let latLng;
+let latLngs = []; // 이전 여행지를 중복해서 추가하려고 할때 체크하기 위한 용도
+
 let $rateYo = $("#placeRating").rateYo({
   rating: 0,
   readOnly: true
-});;
+});
+
+const placeNmEl = document.getElementById("placeNm");
+const placeAddrEl = document.getElementById("placeAddr");
+const placePhotoEl = document.getElementById("placePhoto");
+
+let tripData = {
+  tripTitle: '',
+  author: '김재현',
+  tripSchedule: [
+    // {
+    //   placeNm: '',
+    //   placeAddr: '',
+    //   placeRating: '',
+    //   placePhoto: '',
+    // }
+  ],
+  mapInfo: [
+    // day 1
+    // {
+    //   markersFinal,
+    //   path,
+      // polys,
+      // latLngs
+    // }
+  ]
+};
 
 function initMap() {
   const incheon = new google.maps.LatLng(37.45639, 126.70528);
+  const zoom = 15;
+
   map = new google.maps.Map(document.getElementById("map"), {
     center: incheon,
-    zoom: 15,
+    zoom,
+    mapTypeId: google.maps.MapTypeId.HYBRID
+  });
+
+  mapOverview = new google.maps.Map(document.getElementById("mapOverview"), {
+    center: incheon,
+    zoom
   });
 
   infoWindow = new google.maps.InfoWindow();
   service = new google.maps.places.PlacesService(map);
+  geocoder = new google.maps.Geocoder();
+
+  // 심볼 정의
+  lineSymbol = {
+    path: google.maps.SymbolPath.FORWARD_CLOSED_ARROW,
+    scale: 5,
+    strokeColor: "#ffff00",
+  };
+
+  // 경로 라인 생성
+  poly = new google.maps.Polyline({
+    // path,
+    icons: [
+      {
+        icon: lineSymbol,
+        offset: "100%",
+      },
+    ],
+    strokeColor: "#0000ff",
+    strokeOpacity: 0.6,
+    strokeWeight: 5,
+  });
+  poly.setMap(mapOverview);
+  // 경로 그리기
+  animateCircle(poly);
 
   // 맵에 클릭 이벤트 리스너 등록
   map.addListener("click", (event) => {
-    // console.log(event);
+    console.log(event);
+
+    // 선택한 곳 위도, 경도 정보 전역으로 저장
+    latLng = event.latLng;
+
+    // 이전 마커 초기화 후 새 마커 추가
+    deleteMarkers();
     addMarker(event.latLng);
 
-    // 장소 정보
-    const request = {
-      placeId: event.placeId,
-      // fields: ["name", "formatted_address", "place_id", "geometry"],
-    };
-
-    service.getDetails(request, (place, status) => {
-      if (
-        status === google.maps.places.PlacesServiceStatus.OK &&
-        place &&
-        place.geometry &&
-        place.geometry.location
-      ) {
-        console.log(status);
-        console.log(place);
-
-        const placeNmEl = document.getElementById("placeNm");
-        const placeAddrEl = document.getElementById("placeAddr");
-        const placePhotoEl = document.getElementById("placePhoto");
-        
-        placeNmEl.value = place.name;
-        placeAddrEl.value = place.formatted_address;
-        $rateYo.rateYo("rating", place.rating || 0);
-        placePhotoEl.setAttribute("src", place.photos && place.photos[0].getUrl() || "https://svgshare.com/i/AY2.svg");
-
-        // const marker = new google.maps.Marker({
-        //   map,
-        //   position: place.geometry.location,
-        // });
-
-        // google.maps.event.addListener(marker, "click", () => {
-        //   const content = document.createElement("div");
-        //   const nameElement = document.createElement("h2");
-
-        //   nameElement.textContent = place.name;
-        //   content.appendChild(nameElement);
-
-        //   const placeIdElement = document.createElement("p");
-
-        //   placeIdElement.textContent = place.place_id;
-        //   content.appendChild(placeIdElement);
-
-        //   const placeAddressElement = document.createElement("p");
-
-        //   placeAddressElement.textContent = place.formatted_address;
-        //   content.appendChild(placeAddressElement);
-        //   infowindow.setContent(content);
-        //   infowindow.open(map, marker);
-        // });
-      }
-    });
+    // place 정보가 있는 곳과 없는 곳을 나눠서 처리
+    let request;
+    if (event.placeId) {
+      request = {
+        placeId: event.placeId,
+        // fields: ["name", "formatted_address", "place_id", "geometry"],
+      };
+      getPlaceInformation(request);
+    } else {
+      request = {
+        location: event.latLng
+      };
+      getAddressInformation(request);
+    }
   });
 
   // 현재 내 위치 찾기
@@ -81,7 +123,6 @@ function initMap() {
   locationButton.classList.add("custom-map-control-button");
   map.controls[google.maps.ControlPosition.TOP_CENTER].push(locationButton);
   locationButton.addEventListener("click", () => {
-    // Try HTML5 geolocation.
     if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition(
         (position) => {
@@ -106,9 +147,112 @@ function initMap() {
   });
 }
 
+// 장소 정보 구하기
+function getPlaceInformation(request) {
+  service.getDetails(request, (place, status) => {
+    // console.log(status);
+    console.log(place);
+
+    if (
+      status === google.maps.places.PlacesServiceStatus.OK &&
+      place &&
+      place.geometry &&
+      place.geometry.location
+    ) {
+      placeNmEl.value = place.name;
+      placeAddrEl.value = place.formatted_address;
+      $rateYo.rateYo("rating", place.rating || 0);
+      placePhotoEl.setAttribute("src", place.photos && place.photos[0].getUrl() || "https://svgshare.com/i/AY2.svg");
+
+      // path 배열 만들기
+      // path.push({
+      //   lat: place.geometry.location.lat(),
+      //   lng: place.geometry.location.lng()
+      // });
+    } else {
+      // placeId는 존재하는데 place 정보가 존재하지 않을 경우 예외처리
+      getAddressInformation({ location: latLng });
+    }
+  });
+}
+
+// 장소 정보가 없는 곳의 주소 정보 구하기
+function getAddressInformation(request) {
+  geocoder.geocode(request, (results, status) => {
+    console.log(results);
+    if (status == 'OK') {
+      if (results[0]) {
+        let infowindow = new google.maps.InfoWindow({
+          content: '주소: ' + results[0].formatted_address
+        });
+        infowindow.open(map, marker);
+        
+        placeNmEl.value = "정보 없음";
+        placeAddrEl.value = results[0].formatted_address;
+        $rateYo.rateYo("rating", 0);
+        placePhotoEl.setAttribute("src", "https://svgshare.com/i/AY2.svg");
+      } else {
+        window.alert('No results found');
+      }
+    } else {
+      //alert('Geocode was not successful for the following reason: ' + status);
+        
+      // 이상한 바다 한가운데 아무런 정보도 없는곳 찍었을 때 null값 들어가는 에러 해결
+      placeNmEl.value = "정보 없음";
+      placeAddrEl.value = "정보 없음";
+      $rateYo.rateYo("rating", 0);
+      placePhotoEl.setAttribute("src", "https://svgshare.com/i/AY2.svg");
+    }
+  });
+}
+
+// 여행지 추가하기
+function addPlace() {
+  // 맵을 클릭하여 위도, 경도 정보가 존재할때만 여행지 추가 로직 진행
+  if (latLng != null && latLng != "") {
+    // 이전에 선택한 여행지를 중복으로 추가못하게 막기
+    if (latLngs[latLngs.length - 1] == latLng) {
+      alert("방금 등록하신 여행지입니다. 다른 여행지를 선택해 주세용! +_+");
+      return;
+    }
+    latLngs.push(latLng);
+  
+    // 맵 클릭시 새로운 경로 라인 정보 추가
+    addLatLng(latLng);
+  
+    marker = new google.maps.Marker({
+      position: latLng,
+      map: mapOverview,
+      animation: google.maps.Animation.BOUNCE
+    });
+    markersFinal.push(marker);
+    
+    mapOverview.setCenter(latLng);
+    mapOverview.setZoom(17);
+
+    // tripData에 추가
+    tripData.tripSchedule.push({
+      placeNm: placeNmEl.value,
+      placeAddr: placeAddrEl.value,
+      placeRating: $rateYo.rateYo("rating"),
+      placePhoto: placePhotoEl.getAttribute("src"),
+    });
+  } else {
+    alert("가고싶은 여행지를 먼저 선택하세요~ !" + "(\'(\")\')!");
+  }
+}
+
+// 경로 라인 정보 추가하기
+function addLatLng(location) {
+  path = poly.getPath(); // path 정보 담을 배열 객체 가져옴
+  path.push(location);
+  polys.push(location); // 위도, 경도를 배열에 담기
+  polyIndex++;
+}
+
 // Adds a marker to the map and push to the array.
 function addMarker(position) {
-  const marker = new google.maps.Marker({
+  marker = new google.maps.Marker({
     position,
     map,
   });
@@ -116,27 +260,36 @@ function addMarker(position) {
   markers.push(marker);
 }
 
-// Sets the map on all markers in the array.
 function setMapOnAll(map) {
   for (let i = 0; i < markers.length; i++) {
     markers[i].setMap(map);
   }
 }
 
-// Removes the markers from the map, but keeps them in the array.
 function hideMarkers() {
   setMapOnAll(null);
 }
 
-// Shows any markers currently in the array.
 function showMarkers() {
   setMapOnAll(map);
 }
 
-// Deletes all markers in the array by removing references to them.
 function deleteMarkers() {
   hideMarkers();
   markers = [];
+}
+
+function animateCircle(line) {
+  let count = 0;
+
+  window.setInterval(() => {
+    count = (count + 1) % 200;
+
+    const icons = line.get("icons");
+
+    icons[0].offset = count / 2 + "%";
+    line.set("icons", icons);
+  }, 20);
 }
 
 function handleLocationError(browserHasGeolocation, infoWindow, pos) {
@@ -162,3 +315,14 @@ function handleLocationError(browserHasGeolocation, infoWindow, pos) {
 //     infoWindow.open(map);
 //   });
 // }
+
+function saveTrip() {
+  if (typeof(Storage) !== "undefined") {
+    tripData.tripTitle = document.getElementById("tripTitle").value;
+    console.log(tripData);
+
+    localStorage.setItem("tripData", JSON.stringify(tripData));
+  } else {
+    alert("Sorry! No Web Storage support..");
+  }
+}
